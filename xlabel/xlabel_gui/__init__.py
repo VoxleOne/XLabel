9da13ccc828bdf30261864ecf175aa23f81be910
ebmarques <git@leilomax.com.br>
2025-06-18 17:08:29 -0300

    Update __init__.py

--- xlabel/xlabel_gui/__init__.py
+++ xlabel/xlabel_gui/__init__.py
@@ -1,1 +1,8 @@
-# XLabel GUI package
+# __init__.py
+
+# Optionally expose core functions here for easier imports.
+# For example:
+# from .creator import embed_xlabel_data
+# from .reader import read_xlabel_data
+
+__version__ = "0.3.1"# XLabel GUI package
